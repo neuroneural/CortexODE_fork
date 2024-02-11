0da0837af649c860bf84e85840ab0b1e5fa636a5
William Ashbee <wsashbee@gmail.com>
2024-02-11 15:45:00 -0500

    benchmarks working

---
+++ singularity/benchmarks/rk4/bm.pred2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /opt/miniconda3/bin/activate cortexode
+cd /cortexode
+python bm.pred2.py --test_type='pred' --data_dir='/speedrun/cortexode-data-rp/bm/' --model_dir='/cortexode/ckpts/experiment_3_mod/model/best/' --result_dir='/cortexode/ckpts/rp-hcptrained-testset-bm/' --data_name='hcp' --surf_hemi='lh' --tag='hcp-trained' --solver='rk4' --step_size=0.1 --device='gpu'
